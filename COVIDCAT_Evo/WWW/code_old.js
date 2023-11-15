77c92e683af672efc9a3c994cc67ad0677be2228
pasahe <37337655+pasahe@users.noreply.github.com>
2023-11-15 11:18:26 +0100

    Delete COVIDCAT_Evo/WWW/code_old.js

--- COVIDCAT_Evo/WWW/code_old.js
+++
@@ -1,3 +0,0 @@
-$( document ).ready(function() {
-  $( ".navbar .container-fluid" ).append('<div><img src=\"logo.png\" alt=\"alt\" style=\"float:right;width:95px;height:50px;padding-top:5px;\"></div>');
-});
